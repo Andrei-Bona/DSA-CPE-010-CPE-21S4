e196ecf00e3b3775f62e9ccc89eeec785a5a5279
Andrei-Bona <149856450+Andrei-Bona@users.noreply.github.com>
2024-09-26 22:02:40 +0800

    Update Exercise5_DSA.cpp

--- Exercise5_DSA.cpp
+++ Exercise5_DSA.cpp
@@ -1,5 +1,4 @@
 //Stack
-/*
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -10,8 +9,6 @@ void display(stack<string>pl)
     {
         cout<<pl.top()<<endl;
         pl.pop();
-        
-
     }
 }
 
@@ -33,49 +30,54 @@ int main ()
 
     return 0;
 }
-*/
 
-//Queue (not finish)
-/*
+// Queue
 #include <iostream>
 #include <queue>
 using namespace std;
 
-void display(queue<string>pl)
+const int MAX_SIZE = 3;
+
+void display(queue<string> pl)
 {
     while (!pl.empty())
     {
-        cout<<pl.front()<<endl;
-        pl.pop();  
-    }
-
-void check(queue<string>pl)
-{
-    if (pl.size() == 3)
-    {
-        cout<<"Queue is Full";
+        cout << pl.front() << endl;
+        pl.pop();
     }
 }
 
-int main ()
+int main()
 {
     queue<string> pl;
-
+    
     pl.push("C++");
     pl.push("Java");
     pl.push("Python");
-    cout<<"Initial Queue:"<<endl;
-    display(pl);
-    //check(pl);
 
-    
-    //*cout<<"Final Queue:"<<endl;
-    pl.pop();
-    cout<<pl.front();*/
+    if (pl.size() == MAX_SIZE)
+    {
+        cout << "The queue is full" << endl;
+    }
+
+    cout << "Initial Queue:" << endl;
+    display(pl);
+
+
+    if (pl.size() < MAX_SIZE)
+    {
+        pl.push("C#");
+    }
+    else
+    {
+        cout << "Cannot add 'C#', the queue is full" << endl;
+    }
+
+
+    cout << "Final Queue after pop:" << endl;
+    pl.pop(); 
+    display(pl);
 
     return 0;
 }
-*/
-
-
 
