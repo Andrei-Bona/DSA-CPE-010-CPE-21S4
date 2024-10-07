cefc1f4eacda18f30d40aa81c7632674a867dc49
Andrei-Bona <149856450+Andrei-Bona@users.noreply.github.com>
2024-10-07 14:58:28 +0800

    Create Queues using C++ STL.cpp

---
+++ Hands-on Activity 5.1 Queues/Queues using C++ STL.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+string array[] = {"Vincent", "Frederick","Andrei", "Anna"}; 
+int i = 0;
+
+void display(queue <string> students)
+{
+    while (!students.empty())
+    {
+        cout << students.front() << endl;
+        students.pop();
+    }
+    cout <<"\n";
+}
+
+int main()
+{
+    queue <string> students;
+
+    cout << "The students in queue is :\n";
+    
+    for (i = 0; i < 4; i++)
+    {
+        students.push(array[i]);
+    }
+    display(students);
+    
+    cout << "students.empty() : " << students.empty() << "\n";
+    cout << "students.size() : " << students.size() << "\n";
+    cout << "students.front() : " << students.front() << "\n";
+    cout << "students.back() : " << students.back() << "\n";
+    cout <<"\n";
+    cout << "students.pop() : \n";
+    students.pop();
+    display(students);
+
+    students.push("Bona");
+    cout << "The students in queue is:\n";
+    display(students);
+
+    
+    return 0;
+}
