cc7156622b63ad70952a7079ce1a4de0f6b8bd44
Andrei-Bona <149856450+Andrei-Bona@users.noreply.github.com>
2024-10-16 14:47:46 +0800

    Add files via upload

---
+++ Hands-on Activity 7.1 Sorting Algorithms/main.cpp
@@ -0,0 +1,41 @@
+// Insertion Sort Algorithm
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "sort.h"
+using namespace std;
+
+int main()
+{
+    const int max_size = 100;
+    
+    // generate random values
+    int arr[max_size];
+    srand(time(0));
+    for (int i = 0; i < max_size; i++) 
+	{
+        arr[i] = rand() % 100;
+    }
+
+    // show your array content
+    cout << "Unsorted array: " <<endl;
+    for (int i = 0; i < max_size; i++) 
+	{
+        cout << arr[i] << " ";
+    }
+
+    size_t arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    cout <<endl;
+    cout <<endl;
+    
+//    bubbleSort(arr, arrSize);
+//    selectionSort(arr, arrSize);
+    insertionSort(arr, arrSize);
+    cout << "Sorted array: " << endl;
+    for (size_t i = 0; i < arrSize; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
